fbc8ebee258e751f85eeaa906b055a6d8b2bd941
Deep Majumder <deep.majumder2019@gmail.com>
2022-10-16 00:13:44 +0530

    Added git directory checking

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (hasGitDirectory) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Monad.Extra (anyM)
+import           System.Directory    (listDirectory)
+import           System.FilePath     (splitPath)
+import           System.Posix.Files  (getFileStatus, isDirectory)
+
+isGitDirectory :: FilePath -> IO Bool
+isGitDirectory p = do
+    fs <- getFileStatus p
+    return (isDirectory fs && checkGit p)
+    where checkGit pp = last (splitPath pp) == ".git"
+
+hasGitDirectory :: FilePath -> IO Bool
+hasGitDirectory p = listDirectory p >>= anyM isGitDirectory
